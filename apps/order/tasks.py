3ce207254daeab22006597e5149d5e7bc4278149
Iskenderovo3o3o <120571187+Iskenderovo3o3o@users.noreply.github.com>
2023-06-05 17:57:39 +0600

    django-shop

---
+++ apps/order/tasks.py
@@ -0,0 +1,21 @@
+from django.core.mail import send_mail
+from celery import shared_task
+
+@shared_task
+def send_confirmation_mail(username,address,pk,email):
+    message = f""" 
+    Здравствуйте, {username}!
+    Потдвердите заказ на адрес {address},
+
+    http://localhost:8000/order/{pk}/confirm/
+
+    Если это были не вы,игнорируйте это сообщение
+    """
+    send_mail(
+        subject='Подтверждение заказа',
+        message=message,
+        from_email='test@test.com',
+        recipient_list=[email],
+        fail_silently=False
+
+    )
